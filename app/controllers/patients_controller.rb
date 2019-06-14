972cbca34793f830b06ed174a54ff41c29af2ec1
Pedro Sordo <pedro.asgarza@gmail.com>
2019-06-13 23:03:58 -0500

    add unfinished patient controller specs

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,36 +1,39 @@
 class PatientsController < ApplicationController
 
   def index
-    @patients = Patient.all
-    render json: @patients
+    patients = Patient.all
+    render json: patients
   end
 
   def show
-    @patient = Patient.find(params[:id])
-    render json: @patient
+    patient = Patient.find(params[:id])
+    render json: patient
+    ### TODO: RESCUE RECORDNOTFOUND
   end
 
   def create
-    @patient = Patient.new(patient_params)
-    if @patient.save
-      render json: @patient
+    patient = Patient.new(patient_params)
+    if patient.save
+      render json: patient
     else
-      render json: @patient.errors
+      render json: patient.errors
     end
   end
 
   def update
-    @patient = Patient.find(params[:id])
-    if @patient.update(patient_params)
-      render json: @patient
+    patient = Patient.find(params[:id])
+    if patient.update(patient_params)
+      render json: patient
     else
-      render json: @patient.errors
+      render json: patient.errors
     end
+    ### TODO: RESCUE RECORDNOTFOUND
   end
 
   def destroy
-    @patient = Patient.find(params[:id])
-    @patient.destroy
+    patient = Patient.find(params[:id])
+    patient.destroy
+    ### TODO: RESCUE RECORDNOTFOUND
   end
 
   private
--- spec/controllers/patients_controller_spec.rb
+++ spec/controllers/patients_controller_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe PatientsController, type: :controller do
+  let!(:patients) { create_list(:patient, 10) }
+  let(:patient_id) { patients.first.id }
 
+  describe 'GET /patients' do
+    before { get :index }
+
+    it 'returns patients' do
+      expect(JSON.parse(response.body)).not_to be_empty
+      expect(JSON.parse(response.body).size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /patient/:id' do
+    before { get :show, params: { id: patient_id}}
+
+    context 'when patient exists' do
+      it 'returns patient' do
+        expect(JSON.parse(response.body)).not_to be_empty
+        expect(JSON.parse(response.body)['id']).to eq(patient_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    # TODO 
+    # context 'when patient doesnt exist' do
+    #   let(:todo_id) { 1021 }
+    #
+    #   it 'returns status code 404' do
+    #     expect(response).to have_http_status(404)
+    #   end
+    #
+    #   it 'returns a not found message' do
+    #     expect(response.body).to match(/Couldn't find Todo/)
+    #   end
+    # end
+  end
 end

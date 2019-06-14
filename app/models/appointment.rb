b71d50ad4ac34f4ffeb0eecee1ea83b3704d6b05
Pedro Sordo <pedro.asgarza@gmail.com>
2019-06-13 21:58:21 -0500

    add fake reminder concer and callbacks to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,18 @@
 class Appointment < ApplicationRecord
+  include FakeReminder
   belongs_to :physician
   belongs_to :patient
 
   validates :cause, presence: true
+
+  after_create :send_notification, if: :reminder?
+
+  def send_notification
+    send_reminder('patient', patient, physician)
+    send_reminder('physician', patient, physician)
+  end
+
+  def reminder?
+    reminder
+  end
 end
